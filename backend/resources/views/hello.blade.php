b6f9b4a4b3002692564820041d854ca3f49489ac
Saburi Yuta <catdogbird1192@gmail.com>
2022-09-10 19:57:20 +0900

    learn git hoo!

--- backend/resources/views/hello.blade.php
+++ backend/resources/views/hello.blade.php
@@ -6,6 +6,7 @@
         <h1>HELLO Laravel<h1>
             <h2>DBから表示中 gitの更新が反映できるか確認！！</h2>
                 <h3>git pull　で更新する方法を模索中</h3>
+                <h4>認証機能を追加したい。。</h4>
                 <a href="{{ route('welcome') }}">welcomeへ</a>
             <tr><th>Name</th><th>Mail</th><th>Age</th></tr>
                 <br>
